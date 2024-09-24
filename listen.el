868bafa2051a99609fa5a34f2e437a1ff5f8f1b4
Adam Porter <adam@alphapapa.net>
2024-09-23 20:16:49 -0500

    Tidy: Function declarations

--- listen.el
+++ listen.el
@@ -146,10 +146,10 @@ Interactively, uses the default player."
     (setf listen-player nil))
   (listen-mode--update))
 
-(declare-function listen-queue-next "listen-queue")
 (defun listen-next (player)
   "Play next track in PLAYER's queue.
 Interactively, uses the default player."
+  (declare-function listen-queue-next "listen-queue")
   (interactive (list (listen-current-player)))
   (listen-queue-next (map-elt (listen-player-etc player) :queue)))
 
@@ -300,8 +300,6 @@ According to `listen-lighter-format', which see."
     (unless (equal "-1" rating)
       (format "[%s]" (* 5 (string-to-number rating))))))
 
-(declare-function listen-queue-play "listen-queue")
-(declare-function listen-queue-next-track "listen-queue")
 (defun listen-mode--update (&rest _ignore)
   "Play next track and/or update variable `listen-mode-lighter'."
   (let ((playingp (and listen-player (listen--playing-p listen-player)))
