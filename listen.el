d43e82b297156588f061f7f92145d53e95a94974
Kai Bagley <kaibagley@pm.me>
2025-12-18 14:22:06 +0800

    Remove: unused function

--- listen.el
+++ listen.el
@@ -515,8 +515,6 @@ TIME is a string like \"SS\", \"MM:SS\", or \"HH:MM:SS\"."
     ("qap" "from playlist file" listen-queue-add-from-playlist-file
      :transient t)]
    ["Subsonic"
-    ("qvs" "from Subsonic" listen-queue-add-from-subsonic
-     :transient t)
     ("qva" "queue starred" listen-subsonic-queue-starred-tracks
      :transient t)]])
 
